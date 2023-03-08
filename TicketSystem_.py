6100960002a971ca2691afe1a263d1d88bc39625
Seunghee Lee <32346026+oogami-kero@users.noreply.github.com>
2023-03-08 12:46:00 -0700

    Update TicketSystem_.py
    Added function to close tickets and add users to tickets
    Added administrator permission checks for initialization command
    
    ToDo: Ticket Moderation and Remove users from tickets

--- TicketSystem_.py
+++ TicketSystem_.py
@@ -2,17 +2,19 @@ import discord
 import datetime
 import pytz
 import os
-from discord import app_commands, utils
+from discord import app_commands, member, utils
 from discord.ext import commands, tasks
 from discord.utils import get
-from discord.ext.commands import Bot
+from discord.ext.commands import Bot, has_permissions, MissingPermissions
 
 time = datetime.datetime.now()
 mst_now = time.astimezone(pytz.timezone('America/Denver'))
 mst_format= mst_now.strftime("%Y/%m/%d %H:%M:%S")
 
-# token here
+# TOKEN HERE
 # TOKEN = ''
+
+# Bot Initialization
 
 class aclient(discord.Client):
   # sync slash commands
@@ -25,23 +27,25 @@ class aclient(discord.Client):
   async def on_ready(self):
     await self.wait_until_ready()
     if not self.synced:
-      await cogs.sync(guild = discord.Object(id = 1067969922258456626))
+      await cogs.sync()
       self.synced = True
 
     # make ticket button persist over restarts  
     if not self.added:
       self.add_view(ticket_launcher())
+      self.add_view(ticket_control())
       self.added = True
 
     # print ready message and set bot status  
     await bot.change_presence(activity=discord.Game('TICKET SYSTEM PROGRESS ■■■■■■□□□□ 60%'))
     print(f'Bot connected as {bot.user}')
     print(mst_format)
-  
-
 
 bot = aclient()
 cogs = app_commands.CommandTree(bot)
+
+#------------------------------------------------------------------------------------------------------#
+
 
 class ticket_launcher(discord.ui.View):
   def __init__(slef) -> None:
@@ -74,18 +78,71 @@ class ticket_launcher(discord.ui.View):
       
       # create ticket channel
       channel = await interaction.guild.create_text_channel(name = f"ticket-for-{interaction.user.name}-{interaction.user.discriminator}",
-                                                            overwrites = overwrites,
-                                                            reason = f"Ticket for {interaction.user}")
-      await channel.send(f"{interaction.user.mention} created a ticket!")
+                                                            overwrites = overwrites, reason = f"Ticket for {interaction.user}")
+      await channel.send(f"{interaction.user.mention} created a ticket!", view = ticket_control())
       await interaction.response.send_message(f"I've opened a ticket for you at {channel.mention}!", ephemeral = True)
 
+# ticket deletion confirmation button
+class confirm(discord.ui.View):
+  def __init__(self) -> None:
+    super().__init__(timeout = None)
+
+  @discord.ui.button(label = "Yes, I'm sure", style = discord.ButtonStyle.red, custom_id = "confirm")
+  async def confirm_button(self, interaction, button):
+    try:
+      await interaction.channel.delete()
+    except:
+      await interaction.response.send_message("Channel could not be deleted, ensure 'manage_channels' permission is given!",
+                                              ephemeral = True)
+
+class ticket_control(discord.ui.View):
+  def __init__(self) -> None:
+    super().__init__(timeout = None)
+
+  # close ticket embed
+  @discord.ui.button(label = "Close this Ticket", style = discord.ButtonStyle.red, custom_id = "close")
+  async def close(self, interaction, button):
+    embed = discord.Embed(title = "Are you sure you want to close this ticket?", color = discord.Colour.brand_red())
+    await interaction.response.send_message(embed = embed, view = confirm(), ephemeral = True)
+
+#------------------------------------------------------------------------------------------------------#
+
+# Slash Commands
+
 # ticket system initialization command
-@cogs.command(guild = discord.Object(id = 1067969922258456626), name = 'ticket', description='launches the ticketing system')
+@cogs.command(name = 'ticket', description = 'Initializes the ticketing system')
+@has_permissions(administrator = True)
 async def ticketing(interaction: discord.Interaction):
   embed = discord.Embed(title = "If you need support, click the button below to create a new ticket!",
-                        color = discord.Colour.teal())
+                        color = discord.Colour.dark_teal())
   await interaction.channel.send(embed = embed, view = ticket_launcher())
   await interaction.response.send_message("Ticketing system initiated successfully", ephemeral = True)
 
+# manual ticket close command
+@cogs.command(name = 'close', description = 'Closes a ticket manually')
+async def close(interaction: discord.Interaction):
+  if "ticket-for-" in interaction.channel.name:
+    embed = discord.Embed(title = "Are you sure you want to close this ticket?", color = discord.Colour.brand_red())
+    await interaction.response.send_message(embed = embed, view = confirm(), ephemeral = True)
+  else:
+    await interaction.response.send_message("This can only be used in a ticket channel", ephemeral = True)
+
+# add user to ticket command
+@cogs.command(name = 'add', description = 'Adds user to existing ticket')
+@app_commands.describe(user = "User you want to add to the ticket")
+async def add(interaction: discord.Interaction, user: discord.Member):
+  if "ticket-for-" in interaction.channel.name:
+    await interaction.channel.set_permissions(user, view_channel = True, send_messages = True,
+                                              attach_files = True, embed_links = True)
+    await interaction.response.send_message(f"{user.mention} has been added to the ticket by {interaction.user.mention}!")
+  else:
+    await interaction.response.send_message("This can only be used in a ticket channel", ephemeral = True)
+  
+
+@cogs.error
+async def command_error(bot, error):
+    if isinstance(error, MissingPermissions):
+        text = "You do not have permissions to do that!"
+        await bot.send_message(bot.message.channel, text, ephemeral = True)
 
 bot.run(TOKEN)
